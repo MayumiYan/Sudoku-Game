4e1d129abb5672124e927a3e075e39f4d1d701d2
Lotus Yoga <Lotus@Lotus-MacBook-Pro.local>
2024-08-22 13:50:30 -0700

    script.js set up and test, file is succesfully connected to HTML

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const newGameButton = document.getElementById('new-game');
+    newGameButton.addEventListener('click', function () {
+        console.log('New Game button clicked!');
+    });
+});  
